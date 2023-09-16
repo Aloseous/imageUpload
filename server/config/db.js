dfd5b63d22eb281349e0a464c8e7812aa9e2ce42
Aloseous Kingsly <50615352+Aloseous@users.noreply.github.com>
2023-09-16 20:39:49 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () =>{
     try{
     const connection = await mongoose.connect(process.env.CONNECTION_URL)
+    console.log('connection -->',connection)
     }catch(err) {
         console.log('Error in connection')
     }
